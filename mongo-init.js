e7fbf43a0ad8c96b66ecdb1e66b909c0c5200f7d
Paulo Ferreira_CONTR <C0655173@vale.com>
2024-05-06 19:56:13 -0300

    Atualizar configurações de conexão com o MongoDB

---
+++ mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+      user: process.env.MONGO_USERNAME,
+      pwd: process.env.MONGO_PASSWORD,
+      roles: [
+        {
+          role: "readWrite",
+          db: process.env.MONGO_DATABASE
+        }
+      ]
+    }
+  );
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,18 @@ import mongoose from 'mongoose';
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/nome_do_banco')
+const dbUser = process.env.MONGO_USERNAME || 'app-tarefas';
+const dbPassword = process.env.MONGO_PASSWORD || 'tarefas123';
+const dbName = process.env.MONGO_DATABASE || 'app_tarefas';
+
+const mongoConnection = `mongodb://${dbUser}:${dbPassword}@mongo:27017/${dbName}`;
+
+mongoose.connect(mongoConnection)
 .then(() => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 }).catch((error) => {
-  console.log(error);
+    console.log(mongoConnection);    
+    console.log(error);
 });
